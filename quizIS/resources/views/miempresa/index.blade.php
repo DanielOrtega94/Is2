199360d5e04c9bfb63c770d7ea110add2464f131
DanielOrtega94 <danielortegacarcamo@gmail.com>
2018-07-12 22:44:53 -0400

    asdasd

--- quizIS/resources/views/miempresa/index.blade.php
+++ quizIS/resources/views/miempresa/index.blade.php
@@ -35,7 +35,7 @@
                     <td>{{ $empresa->direccion }}</td>
                     <td>{{ $empresa->telefono }}</td>
                     <td>{{ $empresa->etapa }}</td>
-                    <td>{{ $empresa->nota }}</td>  
+                    <td>{{ $empresa->nota }}/7</td>  
                     <td>
                         <a href="{{ route('miempresa.show',[$empresa->id]) }}" class="btn btn-xs btn-primary">@lang('quickadmin.view')</a>
                         <a href="{{ route('miempresa.edit',[$empresa->id]) }}" class="btn btn-xs btn-info">@lang('quickadmin.edit')</a>
